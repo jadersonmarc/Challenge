688bb41eaaa7f8eeca0c2193bb537cf5e02c6157
jadersonmarc <jadersonmarc@gmail.com>
2020-09-07 17:03:33 -0300

    start  blog

---
+++ blog-frontend/src/main.js
@@ -0,0 +1,18 @@
+import '@babel/polyfill'
+import 'mutationobserver-shim'
+import Vue from 'vue'
+import './plugins/axios'
+import './plugins/bootstrap-vue'
+import App from './App.vue'
+import vuetify from './plugins/vuetify';
+import store from './store'
+import router from './router'
+
+Vue.config.productionTip = false
+
+new Vue({
+  vuetify,
+  store,
+  router,
+  render: h => h(App)
+}).$mount('#app')
